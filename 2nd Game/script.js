14e46602885e7fd27f2c9bab3b2269744edd20a9
JackBurch <31357591+JackBurch@users.noreply.github.com>
2018-01-17 10:03:45 -0600

    Game2
    second game start

---
+++ 2nd Game/script.js
@@ -0,0 +1,9 @@
+function setup () {
+    background(255);
+    createCanvas(1280, 720);
+}
+function draw(){
+    background(147,70,255);
+    createImg('http://www.freakingnews.com/pictures/9000/Donald-Trump--9270.jpg');
+    img(100,100);
+}
